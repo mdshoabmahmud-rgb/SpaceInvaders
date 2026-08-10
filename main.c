0c68b29713418c403580117dfd7758988b50d191
mdshoabmahmud-rgb <mdshoabmahmud@gmail.com>
2026-08-10 22:58:02 +0600

    Implement alien movement and hero controls
    Added alien movement functionality and hero controls.

--- main.c
+++ main.c
@@ -1,54 +1,90 @@
 #include "raylib.h"
 #include "raymath.h"
-#define AlienSize 50
 #define WindowWidth 1500
 #define WindowHeight 900
+#define AlienSize 50
 #define AlienDistance 50
-int main(void)
-{
-    int AlienInX = (WindowWidth/(AlienSize+AlienDistance))-2;
-    int AlienInY = (WindowHeight/(2*(AlienSize+AlienDistance)))-1;
-    Vector2 AlienPos[AlienInX][AlienInY];
-    InitWindow(WindowWidth, WindowHeight, "Space Invaders");
-    SetTargetFPS(60);
-    AlienPos[0][0] = (Vector2){ 150, 50 };
-    Vector2 AlienSpeedX= { 70, 0 };
-    for (int AlienPosXDex =0; AlienPosXDex < AlienInX; AlienPosXDex++){
-        for(int AlienPosYDex =0; AlienPosYDex < AlienInY; AlienPosYDex++){ 
-            AlienPos[AlienPosXDex][AlienPosYDex] = (Vector2){ AlienPos[0][0].x + (AlienSize+AlienDistance)*AlienPosXDex, AlienPos[0][0].y + (AlienSize+AlienDistance)*AlienPosYDex };
+#define AlienSpeedX 70
+#define AlienSpeedY 0
+#define HeroWidth 25
+#define HeroHeight 50
+#define HeroSpeedX 300
+void DownAlien(int AlienInX, int AlienInY, Vector2 AlienPos[AlienInX][AlienInY]){
+    for(int X =0; X < AlienInX; X++){
+        for(int Y =0; Y < AlienInY; Y++){
+            AlienPos[X][Y].y += AlienSize/2;
         }
     }
+}
+int main(void)
+{
+    InitWindow(WindowWidth, WindowHeight, "Space Invaders");
+    SetTargetFPS(60);
+    //Alien speed and position
+    int AlienInX = (WindowWidth/(AlienSize+AlienDistance))-2;
+    int AlienInY = (WindowHeight/(2*(AlienSize+AlienDistance)));
+    Vector2 AlienPos[AlienInX][AlienInY];
+    AlienPos[0][0] = (Vector2){ 150, 50 };
+    Vector2 AlienSpeed= { AlienSpeedX, AlienSpeedY };
+    for (int X =0; X < AlienInX; X++){
+        for(int Y =0; Y < AlienInY; Y++){ 
+            AlienPos[X][Y].x = AlienPos[0][0].x + (AlienSize+AlienDistance)*X;
+            AlienPos[X][Y].y = AlienPos[0][0].y + (AlienSize+AlienDistance)*Y;
+        }
+    }
+    //Hero position and speed
+    Vector2 HeroPos = { WindowWidth/2, WindowHeight-HeroHeight };
+    Vector2 HeroSpeed ={0,0};
+    //Frame Calculation
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(BLACK);
-        //Drawing Aliens
         float Time = GetFrameTime();
-        for(int AlienPosXDex =0; AlienPosXDex < AlienInX; AlienPosXDex++){
-            for(int AlienPosYDex =0; AlienPosYDex < AlienInY; AlienPosYDex++){
-                if (AlienPos[AlienInX-1][AlienPosYDex].x + AlienSize >= WindowWidth && AlienSpeedX.x > 0){
-                    AlienPos[AlienInX-1][AlienPosYDex].x = WindowWidth - AlienSize;
-                    AlienSpeedX.x =AlienSpeedX.x * -1 ;
-                    for(int AlienPosXDex =0; AlienPosXDex < AlienInX; AlienPosXDex++){
-                        for(int AlienPosYDex =0; AlienPosYDex < AlienInY; AlienPosYDex++){
-                    AlienPos[AlienPosXDex][AlienPosYDex].y = AlienPos[AlienPosXDex][AlienPosYDex].y + AlienSize/2;
-                }
-            }
-                }
-                else if (AlienPos[0][AlienPosYDex].x <= 0 && AlienSpeedX.x < 0){
-                    AlienPos[0][AlienPosYDex].x = 0;
-                    AlienSpeedX.x =AlienSpeedX.x * -1 ;
-                    for(int AlienPosXDex =0; AlienPosXDex < AlienInX; AlienPosXDex++){
-                        for(int AlienPosYDex =0; AlienPosYDex < AlienInY; AlienPosYDex++){
-                    AlienPos[AlienPosXDex][AlienPosYDex].y = AlienPos[AlienPosXDex][AlienPosYDex].y + AlienSize/2;
-                }
-            }
-                }
-                AlienPos[AlienPosXDex][AlienPosYDex]=Vector2Add(AlienPos[AlienPosXDex][AlienPosYDex], Vector2Scale(AlienSpeedX, Time));
-                Rectangle Alien = { AlienPos[AlienPosXDex][AlienPosYDex].x, AlienPos[AlienPosXDex][AlienPosYDex].y, AlienSize, AlienSize };
+//movement of aliens
+        if (AlienPos[AlienInX-1][0].x + AlienSize >= WindowWidth && AlienSpeed.x > 0)
+        {
+            AlienSpeed.x *= -1 ;
+            DownAlien(AlienInX, AlienInY, AlienPos);
+        }
+        else if (AlienPos[0][0].x <= 0 && AlienSpeed.x < 0)
+        {
+            AlienSpeed.x *= -1 ;
+            DownAlien(AlienInX, AlienInY, AlienPos);
+        }
+//movement of aliens end
+//Drawing of aliens 
+        for(int X =0; X < AlienInX; X++){
+            for(int Y =0; Y < AlienInY; Y++){
+                AlienPos[X][Y]=Vector2Add(AlienPos[X][Y], Vector2Scale(AlienSpeed, Time));
+                Rectangle Alien = { AlienPos[X][Y].x, AlienPos[X][Y].y, AlienSize, AlienSize };
                 DrawRectangleRec(Alien, RED);
             }
         }
+//Drawing of Hero and movement
+        if (IsKeyDown(KEY_RIGHT))
+        {
+            HeroSpeed.x = HeroSpeedX;
+        }
+        else if (IsKeyDown(KEY_LEFT))
+        {
+            HeroSpeed.x = -HeroSpeedX;
+        }
+        else
+        {
+            HeroSpeed.x = 0;
+        }
+        HeroPos = Vector2Add(HeroPos, Vector2Scale(HeroSpeed, Time));
+        if (HeroPos.x <= HeroWidth/2)
+        {
+            HeroPos.x = HeroWidth/2;
+        }
+        else if (HeroPos.x + HeroWidth/2 >= WindowWidth)
+        {
+            HeroPos.x = WindowWidth - HeroWidth/2;
+        }
+        Rectangle Hero = { HeroPos.x-HeroWidth/2, HeroPos.y, HeroWidth, HeroHeight };
+        DrawRectangleRec(Hero, BLUE);
         EndDrawing();
     }
     CloseWindow();
