1b01b6e8aec375c0caf0376779380b1b0a561058
mdshoabmahmud-rgb <mdshoabmahmud@gmail.com>
2026-08-11 21:07:20 +0600

    Refactor alien and hero texture handling
    Updated hero and alien textures, adjusted hero dimensions.

--- main.c
+++ main.c
@@ -2,12 +2,16 @@
 #include "raymath.h"
 #define WindowWidth 1500
 #define WindowHeight 900
+
 #define AlienSize 50
 #define AlienDistance 50
 #define AlienSpeedX 20
 #define AlienSpeedY 0
-#define HeroWidth 50
-#define HeroHeight 100
+#define AlienSprite 3
+#define AlienSpriteStyle 2
+
+#define HeroWidth 100//156
+#define HeroHeight 135 //150
 #define HeroSpeedX 500
 void DownAlien(int AlienInX, int AlienInY, Vector2 AlienPos[AlienInX][AlienInY]){
     for(int X =0; X < AlienInX; X++){
@@ -32,6 +36,14 @@ int main(void)
             AlienPos[X][Y].y = AlienPos[0][0].y + (AlienSize+AlienDistance)*Y;
         }
     }
+    Texture2D AlienTexture[AlienSprite][AlienSpriteStyle];
+    Texture2D HeroTexture = LoadTexture("assets/sprites/Hero.png");
+    AlienTexture[0][0] = LoadTexture("assets/sprites/Alien1style1.png");
+    AlienTexture[0][1] = LoadTexture("assets/sprites/Alien1style2.png");
+    AlienTexture[1][0] = LoadTexture("assets/sprites/Alien2style1.png");
+    AlienTexture[1][1] = LoadTexture("assets/sprites/Alien2style2.png");
+    AlienTexture[2][0] = LoadTexture("assets/sprites/Alien3style1.png");
+    AlienTexture[2][1] = LoadTexture("assets/sprites/Alien3style2.png");
     //Hero position and speed
     Vector2 HeroPos = { WindowWidth/2, WindowHeight-HeroHeight };
     Vector2 HeroSpeed ={0,0};
@@ -58,7 +70,15 @@ int main(void)
             for(int Y =0; Y < AlienInY; Y++){
                 AlienPos[X][Y]=Vector2Add(AlienPos[X][Y], Vector2Scale(AlienSpeed, Time));
                 Rectangle Alien = { AlienPos[X][Y].x, AlienPos[X][Y].y, AlienSize, AlienSize };
-                DrawRectangleRec(Alien, RED);
+                int AStyle = (int)(GetTime()/(0.1))%2;
+                DrawTexturePro(
+                    AlienTexture[Y%AlienSprite][AStyle],
+                    (Rectangle){ 0, 0, AlienTexture[Y%AlienSprite][AStyle].width, AlienTexture[Y%AlienSprite][AStyle].height },
+                    Alien, (Vector2){ 0, 0 },
+                    0.0f,
+                    WHITE
+                );
+                
             }
         }
 //Drawing of Hero and movement
@@ -74,6 +94,7 @@ int main(void)
         {
             HeroSpeed.x = 0;
         }
+//Edge detection of Hero
         HeroPos = Vector2Add(HeroPos, Vector2Scale(HeroSpeed, Time));
         if (HeroPos.x <= HeroWidth/2)
         {
@@ -83,9 +104,18 @@ int main(void)
         {
             HeroPos.x = WindowWidth - HeroWidth/2;
         }
+//Drwaing hero
         Rectangle Hero = { HeroPos.x-HeroWidth/2, HeroPos.y, HeroWidth, HeroHeight };
-        DrawRectangleRec(Hero, BLUE);
+        DrawTexturePro(HeroTexture, (Rectangle){ 0, 0, HeroTexture.width, HeroTexture.height }, Hero, (Vector2){ 0, 0 }, 0.0f, WHITE);
         EndDrawing();
+    }
+    UnloadTexture(HeroTexture);
+    for (int Asprite = 0; Asprite < AlienSprite; Asprite++)
+    {
+        for (int AStyle = 0; AStyle < AlienSpriteStyle; AStyle++)
+        {
+            UnloadTexture(AlienTexture[Asprite][AStyle]);
+        }
     }
     CloseWindow();
     return 0;
