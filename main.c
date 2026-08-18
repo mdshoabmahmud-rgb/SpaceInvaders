36bd943e6e7a580b6b658323c8d945e54f7b056c
mdshoabmahmud-rgb <mdshoabmahmud@gmail.com>
2026-08-18 08:29:27 +0600

    Refactor game logic and improve rendering
    Refactor hero and alien movement logic, update bullet mechanics, and improve drawing functions.

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "raylib.h"
 #include "raymath.h"
+#include <stdbool.h>
+
 #define WindowWidth 1500
 #define WindowHeight 900
 
@@ -10,32 +12,54 @@
 #define AlienSprite 3
 #define AlienSpriteStyle 2
 
-#define HeroWidth 100//156
-#define HeroHeight 135 //150
-#define HeroSpeedX 500
-void DownAlien(int AlienInX, int AlienInY, Vector2 AlienPos[AlienInX][AlienInY]){
-    for(int X =0; X < AlienInX; X++){
-        for(int Y =0; Y < AlienInY; Y++){
-            AlienPos[X][Y].y += AlienSize/2;
+#define HeroWidth 100
+#define HeroHeight 135
+#define HeroSpeedX 250
+
+
+#define BulletSpeedY 700
+#define BulletWidth 6
+#define BulletHeight 20
+
+void DownAlien(int AlienInX, int AlienInY, Vector2 AlienPos[AlienInX][AlienInY])
+{
+    for (int X = 0; X < AlienInX; X++)
+    {
+        for (int Y = 0; Y < AlienInY; Y++)
+        {
+            AlienPos[X][Y].y += AlienSize / 2.0f;
         }
     }
 }
+
 int main(void)
 {
     InitWindow(WindowWidth, WindowHeight, "Space Invaders");
     SetTargetFPS(60);
-    //Alien speed and position
-    int AlienInX = (WindowWidth/(AlienSize+AlienDistance))-2;
-    int AlienInY = (WindowHeight/(2*(AlienSize+AlienDistance)));
+
+    // Alien grid setup
+    int AlienInX = (WindowWidth / (AlienSize + AlienDistance)) - 2;
+    int AlienInY = (WindowHeight / (2 * (AlienSize + AlienDistance)));
     Vector2 AlienPos[AlienInX][AlienInY];
+    bool AlienAlive[AlienInX][AlienInY];
+
+    int AlienKilled = 0;
+    int BulletUsed = 0;
+
     AlienPos[0][0] = (Vector2){ 150, 50 };
-    Vector2 AlienSpeed= { AlienSpeedX, AlienSpeedY };
-    for (int X =0; X < AlienInX; X++){
-        for(int Y =0; Y < AlienInY; Y++){ 
-            AlienPos[X][Y].x = AlienPos[0][0].x + (AlienSize+AlienDistance)*X;
-            AlienPos[X][Y].y = AlienPos[0][0].y + (AlienSize+AlienDistance)*Y;
+    Vector2 AlienSpeed = { AlienSpeedX, AlienSpeedY };
+
+    for (int X = 0; X < AlienInX; X++)
+    {
+        for (int Y = 0; Y < AlienInY; Y++)
+        {
+            AlienPos[X][Y].x = AlienPos[0][0].x + (AlienSize + AlienDistance) * X;
+            AlienPos[X][Y].y = AlienPos[0][0].y + (AlienSize + AlienDistance) * Y;
+            AlienAlive[X][Y] = true;
         }
     }
+
+    // Load textures
     Texture2D AlienTexture[AlienSprite][AlienSpriteStyle];
     Texture2D HeroTexture = LoadTexture("assets/sprites/Hero.png");
     AlienTexture[0][0] = LoadTexture("assets/sprites/Alien1style1.png");
@@ -44,44 +68,111 @@ int main(void)
     AlienTexture[1][1] = LoadTexture("assets/sprites/Alien2style2.png");
     AlienTexture[2][0] = LoadTexture("assets/sprites/Alien3style1.png");
     AlienTexture[2][1] = LoadTexture("assets/sprites/Alien3style2.png");
-    //Hero position and speed
-    Vector2 HeroPos = { WindowWidth/2, WindowHeight-HeroHeight };
-    Vector2 HeroSpeed ={0,0};
-    //Frame Calculation
+
+    // Hero position and speed
+    Vector2 HeroPos = { WindowWidth / 2.0f, WindowHeight - HeroHeight };
+    Vector2 HeroSpeed = { 0, 0 };
+
+    // Initializing bullet
+    Vector2 BulletPos = { 0, 0 };
+    bool BulletActive = false;
+
+    
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(BLACK);
         float Time = GetFrameTime();
-//movement of aliens
-        if (AlienPos[AlienInX-1][0].x + AlienSize >= WindowWidth && AlienSpeed.x > 0)
+        
+
+        // Hero shooting logic
+        if (IsKeyPressed(KEY_SPACE))
         {
-            AlienSpeed.x *= -1 ;
+            if (!BulletActive)
+            {
+                BulletActive = true;
+                BulletUsed++;
+                BulletPos = (Vector2){ HeroPos.x, HeroPos.y };
+            }
+        }
+
+        // Bullet movement+alien collisions
+        if (BulletActive)
+        {
+            BulletPos.y -= BulletSpeedY * Time;
+
+            if (BulletPos.y < -BulletHeight)
+            {
+                BulletActive = false;
+            }
+            else
+            {
+                Rectangle bulletRec = { BulletPos.x - BulletWidth / 2.0f, BulletPos.y, BulletWidth, BulletHeight };
+                
+                for (int X = 0; X < AlienInX; X++)
+                {
+                    for (int Y = 0; Y < AlienInY; Y++)
+                    {
+                        if (AlienAlive[X][Y])
+                        {
+                            Rectangle alienRec = { AlienPos[X][Y].x, AlienPos[X][Y].y, AlienSize, AlienSize };
+                            
+                            if (CheckCollisionRecs(bulletRec, alienRec))
+                            {
+                                AlienAlive[X][Y] = false;
+                                AlienKilled++;
+                                BulletActive = false;
+                                break;
+                            }
+                        }
+                    }
+                    if (!BulletActive) break;
+                }
+            }
+        }
+
+        // Movement of aliens
+        if (AlienPos[AlienInX - 1][0].x + AlienSize >= WindowWidth && AlienSpeed.x > 0)
+        {
+            AlienSpeed.x *= -1;
             DownAlien(AlienInX, AlienInY, AlienPos);
         }
         else if (AlienPos[0][0].x <= 0 && AlienSpeed.x < 0)
         {
-            AlienSpeed.x *= -1 ;
+            AlienSpeed.x *= -1;
             DownAlien(AlienInX, AlienInY, AlienPos);
         }
-//movement of aliens end
-//Drawing of aliens 
-        for(int X =0; X < AlienInX; X++){
-            for(int Y =0; Y < AlienInY; Y++){
-                AlienPos[X][Y]=Vector2Add(AlienPos[X][Y], Vector2Scale(AlienSpeed, Time));
-                Rectangle Alien = { AlienPos[X][Y].x, AlienPos[X][Y].y, AlienSize, AlienSize };
-                int AStyle = (int)(GetTime()/(0.1))%2;
-                DrawTexturePro(
-                    AlienTexture[Y%AlienSprite][AStyle],
-                    (Rectangle){ 0, 0, AlienTexture[Y%AlienSprite][AStyle].width, AlienTexture[Y%AlienSprite][AStyle].height },
-                    Alien, (Vector2){ 0, 0 },
-                    0.0f,
-                    WHITE
-                );
-                
+
+        // Drawing of aliens
+        for (int X = 0; X < AlienInX; X++)
+        {
+            for (int Y = 0; Y < AlienInY; Y++)
+            {
+                AlienPos[X][Y] = Vector2Add(AlienPos[X][Y], Vector2Scale(AlienSpeed, Time));
+
+                if (AlienAlive[X][Y])
+                {
+                    Rectangle Alien = { AlienPos[X][Y].x, AlienPos[X][Y].y, AlienSize, AlienSize };
+                    int AStyle = (int)(GetTime() / 0.1) % 2;
+                    DrawTexturePro(
+                        AlienTexture[Y % AlienSprite][AStyle],
+                        (Rectangle){ 0, 0, (float)AlienTexture[Y % AlienSprite][AStyle].width, (float)AlienTexture[Y % AlienSprite][AStyle].height },
+                        Alien, 
+                        (Vector2){ 0, 0 },
+                        0.0f,
+                        WHITE
+                    );
+                }
             }
         }
-//Drawing of Hero and movement
+
+        // Drawing of bullet
+        if (BulletActive)
+        {
+            DrawRectangle((int)(BulletPos.x - BulletWidth / 2.0f), (int)BulletPos.y, BulletWidth, BulletHeight, YELLOW);
+        }
+
+        // Drawing and movement of Hero
         if (IsKeyDown(KEY_RIGHT))
         {
             HeroSpeed.x = HeroSpeedX;
@@ -94,21 +185,26 @@ int main(void)
         {
             HeroSpeed.x = 0;
         }
-//Edge detection of Hero
+
         HeroPos = Vector2Add(HeroPos, Vector2Scale(HeroSpeed, Time));
-        if (HeroPos.x <= HeroWidth/2)
+        if (HeroPos.x <= HeroWidth / 2.0f)
         {
-            HeroPos.x = HeroWidth/2;
+            HeroPos.x = HeroWidth / 2.0f;
         }
-        else if (HeroPos.x + HeroWidth/2 >= WindowWidth)
+        else if (HeroPos.x + HeroWidth / 2.0f >= WindowWidth)
         {
-            HeroPos.x = WindowWidth - HeroWidth/2;
+            HeroPos.x = WindowWidth - HeroWidth / 2.0f;
         }
-//Drwaing hero
-        Rectangle Hero = { HeroPos.x-HeroWidth/2, HeroPos.y, HeroWidth, HeroHeight };
-        DrawTexturePro(HeroTexture, (Rectangle){ 0, 0, HeroTexture.width, HeroTexture.height }, Hero, (Vector2){ 0, 0 }, 0.0f, WHITE);
+
+        Rectangle Hero = { HeroPos.x - HeroWidth / 2.0f, HeroPos.y, HeroWidth, HeroHeight };
+        DrawTexturePro(HeroTexture, (Rectangle){ 0, 0, (float)HeroTexture.width, (float)HeroTexture.height }, Hero, (Vector2){ 0, 0 }, 0.0f, WHITE);
+        DrawRectangleRec((Rectangle){HeroPos.x,0,1 ,WindowHeight-HeroHeight }, RED);
+        DrawText(TextFormat("Aliens Killed: %d", AlienKilled), 10, 10, 20, WHITE);
+
         EndDrawing();
     }
+
+    // Cleanup
     UnloadTexture(HeroTexture);
     for (int Asprite = 0; Asprite < AlienSprite; Asprite++)
     {
@@ -117,6 +213,7 @@ int main(void)
             UnloadTexture(AlienTexture[Asprite][AStyle]);
         }
     }
+    
     CloseWindow();
     return 0;
-} 
+}
